96a95cbbf327463f0c48106fcf9ab945ca95e412
kiteena <kristinamilkovich@gmail.com>
2018-06-23 14:05:51 -0700

    Added GET todo by Id route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose.js');
 let {Todo} = require('./models/todo');
 let {Users} = require('./models/user');
+
 
 var app = express();
 
@@ -28,6 +30,22 @@ app.get('/todos', (req, res)=>{
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }, (err)=>{
+        res.status(400).send();
     });
 });
 
